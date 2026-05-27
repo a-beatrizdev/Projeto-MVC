aeb843c63caea1afb5d4919d580980f005c55597
Alana <alanabeatriztobias@gmail.com>
2026-05-27 12:42:09 -0300

    Continuação do projeto

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from app.auth import get_usuario_opcional
+
 from app.controllers import auth_controller
+from app.controllers import usuario_controller
 
 app = FastAPI(title="Sistema de Ponto de Venda")
 
@@ -16,7 +18,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 # Incluir os routers dos controladores
 app.include_router(auth_controller.router)
-
+app.include_router(usuario_controller.router)
 
 @app.get("/")
 def tela_inicial(
---
+++ criar_usuarios.py
@@ -0,0 +1,41 @@
+# Script para popular o banco de dados com usuários admin
+
+from app.database import Session
+from app.models.usuarios import Usuario
+from app.auth import hash_senha
+
+# Função para cadastrar os usuários
+def seed():
+    db = Session()
+    try:
+        nome_usuario = "admin"
+        email_usuario = "admin@teste.com"
+        senha_usuario = "admin@123"
+        perfil = "admin"
+       
+        # Verificar se o usuário já existe
+        existente = db.query(Usuario).filter_by(email=email_usuario).first()
+
+        if not existente:
+            # Criar o usuário
+            usuario = Usuario(
+                nome=nome_usuario,
+                email=email_usuario,
+                senha_hash=hash_senha(senha_usuario),
+                role=perfil
+                )
+            db.add(usuario)
+            db.commit()
+            print(f"Usuário cadastrado com sucesso {nome_usuario}")
+        else:
+            print(f"Esse e-mail já está cadastrado!")
+
+
+    except Exception as erro:
+        db.rollback()
+        print(f"Erro: {erro}")
+    finally:
+        db.close()
+
+#Chamar a função
+seed()
